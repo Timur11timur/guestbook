7aab08c41db58da2011514b7d6675a02c0449f65
Timur Fralik <timur11timur@gmail.com>
2024-04-08 17:51:36 +0300

    Add Tests

---
+++ tests/Controller/ConferenceControllerTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ConferenceControllerTest extends WebTestCase
+{
+    public function testIndex()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        //echo $client->getResponse();
+
+        $this->assertResponseIsSuccessful();
+        $this->assertSelectorTextContains('h2', 'Give your feedback');
+    }
+}
